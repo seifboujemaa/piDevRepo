d7d48421f2d4daa12aed0f3a419c27ff7536e4be
seifboujemaa <seifeddine.boujemaa@esprit.tn>
2013-10-03 17:40:55 +0100

    first commit
    Signed-off-by: seifboujemaa <seifeddine.boujemaa@esprit.tn>

---
+++ tn.edu.esprit.piDev.artOfDev.liveUp/ejbModule/tn/edu/esprit/piDev/artOfDev/liveUp/services/UserServicesLocal.java
@@ -0,0 +1,8 @@
+package tn.edu.esprit.piDev.artOfDev.liveUp.services;
+
+import javax.ejb.Local;
+
+@Local
+public interface UserServicesLocal {
+
+}
---
+++ tn.edu.esprit.piDev.artOfDev.liveUp/ejbModule/tn/edu/esprit/piDev/artOfDev/liveUp/services/UserServicesRemote.java
@@ -0,0 +1,8 @@
+package tn.edu.esprit.piDev.artOfDev.liveUp.services;
+
+import javax.ejb.Remote;
+
+@Remote
+public interface UserServicesRemote {
+
+}
