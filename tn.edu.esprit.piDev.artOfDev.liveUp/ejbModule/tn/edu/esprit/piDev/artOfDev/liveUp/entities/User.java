48645ec1a64ad1057af14b77f329e01db3f382bc
aya <aya@aya-PC>
2013-10-04 18:29:14 +0200

    Signed-off-by: aya <aya@aya-PC>

---
+++ tn.edu.esprit.piDev.artOfDev.liveUp/ejbModule/tn/edu/esprit/piDev/artOfDev/liveUp/entities/User.java
@@ -0,0 +1,78 @@
+package tn.edu.esprit.piDev.artOfDev.liveUp.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	private int idUser;
+	private String login ;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	private String pwd ;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
+	private static final long serialVersionUID = 1L;
+
+	public User() {
+		super();
+	}
+   
+}
