a6db421c38e273c9528f6bf1a70386b31d5bb934
Zarrad <brhmzrrd@hotmail.fr>
2013-10-11 02:20:14 +0200

    Signed-off-by: Zarrad <brhmzrrd@hotmail.fr>

---
+++ tn.edu.esprit.pidev.artofdev.liveup.client/appClientModule/GUI/AddGame.java
@@ -0,0 +1,226 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import tn.edu.esprit.pidev.artofdev.liveup.client.delegate.GameServicesDelegate;
+import tn.edu.esprit.pidev.artofdev.liveup.client.delegate.StadiumServicesDelegate;
+import tn.edu.esprit.pidev.artofdev.liveup.client.delegate.TeamServicesDelegate;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Game;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Stadium;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Team;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AddGame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField timeField;
+	private JTextField refreeField;
+	private JTextField team1Score;
+	private JTextField team2Score;
+	private List<Team> teams ;
+	private List<Stadium> stadiums ;
+
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddGame frame = new AddGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddGame() {
+		final TeamServicesDelegate teamService = new TeamServicesDelegate();
+		final StadiumServicesDelegate stadiumService = new StadiumServicesDelegate();
+		final GameServicesDelegate gameService = new GameServicesDelegate();
+		
+		teams = new ArrayList<Team>();
+		teams=teamService.findAllTeam();
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 509, 462);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		final JComboBox teamCombo1 = new JComboBox();
+		teamCombo1.setBounds(107, 25, 109, 20);
+		contentPane.add(teamCombo1);
+		for(Team team : teams)
+		{
+			teamCombo1.addItem(team.getName());
+		}
+		
+		
+		final JComboBox teamCombo2 = new JComboBox();
+		teamCombo2.setBounds(107, 84, 109, 20);
+		contentPane.add(teamCombo2);
+		for(Team team : teams)
+		{
+			teamCombo2.addItem(team.getName());
+		}
+		
+		JLabel lblNewLabel = new JLabel("Team 1 ");
+		lblNewLabel.setBounds(32, 28, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Team 2");
+		lblNewLabel_1.setBounds(32, 87, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		final JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(107, 144, 87, 20);
+		contentPane.add(dateChooser);
+		
+		JLabel lblNewLabel_2 = new JLabel("Date");
+		lblNewLabel_2.setBounds(32, 144, 46, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		timeField = new JTextField();
+		timeField.setBounds(108, 197, 86, 20);
+		contentPane.add(timeField);
+		timeField.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Time");
+		lblNewLabel_3.setBounds(32, 200, 46, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		refreeField = new JTextField();
+		refreeField.setBounds(108, 295, 86, 20);
+		contentPane.add(refreeField);
+		refreeField.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Refree");
+		lblNewLabel_4.setBounds(32, 298, 46, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		final JComboBox typeCombo = new JComboBox();
+		typeCombo.setBounds(107, 337, 87, 20);
+		contentPane.add(typeCombo);
+		typeCombo.addItem("Group Stage");
+		typeCombo.addItem("Final Quarter");
+		typeCombo.addItem("3rd Place");
+		typeCombo.addItem("Semi Final");
+		typeCombo.addItem("Final");
+
+		
+		
+		JLabel lblType = new JLabel("Type");
+		lblType.setBounds(32, 340, 46, 14);
+		contentPane.add(lblType);
+		
+		final JComboBox groupCombo = new JComboBox();
+		groupCombo.setBounds(107, 379, 87, 20);
+		contentPane.add(groupCombo);
+		groupCombo.addItem("A");
+		groupCombo.addItem("B");
+		groupCombo.addItem("C");
+		groupCombo.addItem("D");
+		groupCombo.addItem("E");
+		groupCombo.addItem("F");
+		groupCombo.addItem("G");
+		groupCombo.addItem("H");
+		
+		
+		JLabel lblGroup = new JLabel("Group");
+		lblGroup.setBounds(32, 382, 46, 14);
+		contentPane.add(lblGroup);
+		
+		team1Score = new JTextField();
+		team1Score.setBounds(329, 25, 32, 20);
+		contentPane.add(team1Score);
+		team1Score.setColumns(10);
+		
+		team2Score = new JTextField();
+		team2Score.setBounds(329, 87, 32, 20);
+		contentPane.add(team2Score);
+		team2Score.setColumns(10);
+		
+		JLabel lblNewLabel_5 = new JLabel("Team1 goals");
+		lblNewLabel_5.setBounds(245, 28, 69, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Team2 goals");
+		lblNewLabel_6.setBounds(245, 90, 69, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Team team1 = new Team();
+				Team team2 = new Team();
+				team1=teamService.findTeamByName((String)teamCombo1.getSelectedItem());
+				team2=teamService.findTeamByName((String)teamCombo2.getSelectedItem());
+				List<Team> teamsGame = new ArrayList<Team>();
+				teamsGame.add(team1);
+				teamsGame.add(team2);
+				int team1Goal=Integer.parseInt(team1Score.getText());
+				int team2Goal=Integer.parseInt(team2Score.getText());
+				Date date = dateChooser.getDate();
+				String refree = refreeField.getText();
+				String group = (String)groupCombo.getSelectedItem();
+				String type = (String)typeCombo.getSelectedItem();
+				String time = timeField.getText();
+				//Stadium stadium = new Stadium();
+				
+				Game game = new Game();
+				game.setDate(date);
+				game.setGroupe(group);
+				game.setRefree(refree);
+				game.setType(type);
+				game.setTime(time);
+				//game.setStadium(stadium);
+				game.setTeam1Goals(team1Goal);
+				game.setTeam1Goals(team2Goal);
+				game.affectGameToTeams(teamsGame);
+				gameService.create(game);
+				team1.setGame(game);
+				team2.setGame(game);
+
+
+			
+			}
+		});
+		btnNewButton.setBounds(296, 340, 89, 44);
+		contentPane.add(btnNewButton);
+		
+		JComboBox stadiumCombo = new JComboBox();
+		stadiumCombo.setBounds(107, 250, 87, 20);
+		contentPane.add(stadiumCombo);
+		/*for(Stadium stadium : stadiums)
+		{
+			stadiumCombo.addItem(stadium.getName());
+		}*/
+		
+		
+		JLabel lblNewLabel_7 = new JLabel("Stadium");
+		lblNewLabel_7.setBounds(32, 253, 46, 14);
+		contentPane.add(lblNewLabel_7);
+	}
+}
