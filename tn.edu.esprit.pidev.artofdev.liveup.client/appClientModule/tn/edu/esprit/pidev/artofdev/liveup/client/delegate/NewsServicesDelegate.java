c0bdf1eb39647a050dfefae4275af18123a112df
aya <aya@172.16.163.43>
2013-10-19 17:05:45 +0200

    Signed-off-by: aya <aya@172.16.163.43>

---
+++ tn.edu.esprit.pidev.artofdev.liveup.client/appClientModule/tn/edu/esprit/pidev/artofdev/liveup/client/delegate/NewsServicesDelegate.java
@@ -0,0 +1,50 @@
+package tn.edu.esprit.pidev.artofdev.liveup.client.delegate;
+
+import java.util.List;
+
+import tn.edu.esprit.pidev.artofdev.liveup.client.servicelocater.ServiceLocator;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.News;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Player;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.persistences.Team;
+import tn.edu.esprit.pidev.artofdev.liveup.ejb.services.news.NewsServicesRemote;
+
+public class NewsServicesDelegate {
+private static final String jndiName = "ejb:/tn.edu.esprit.pidev.artofdev.liveup.ejb/NewsServices!"+NewsServicesRemote.class.getCanonicalName();
+	
+	private static NewsServicesRemote getProxy(){
+		return (NewsServicesRemote)ServiceLocator
+				.getInstance().
+				 	getProxy(jndiName);
+	}
+	
+	public  void create(News news) {
+		getProxy().createNews(news);
+		
+	}
+	
+	public static void update(News news) {
+		getProxy().updateNews(news);
+		
+	}
+	public static void delete(News news) {
+		getProxy().deleteNews(news);
+		
+	}
+	public static List<News> findAllNews() {
+		 return getProxy().findAllNews();
+		
+	}
+	public static News findNewsById(int idNews) {
+		return getProxy().findNewsById(idNews);
+		
+	}
+	
+	
+	}
+	
+	
+	
+
+	
+
+
